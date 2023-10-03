ccc6747dcd55167864198a58b81e778ae745b91f
olayomipo <adexadexdede@gmail.com>
2023-10-03 17:17:20 +0100

    Django template configured

---
+++ core/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    
+    return render(request, 'core/index.html')
+
+def add(request):
+    
+    return render(request, 'core/add.html')
